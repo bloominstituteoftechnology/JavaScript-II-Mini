38156b4488bbf44c87716ae0af447eec3520ce5d
Ryan Hamblin <ryanhca@gmail.com>
2017-11-08 10:13:11 -0700

    recursion

--- README.md
+++ README.md
@@ -10,3 +10,9 @@
 * This repository is provided as more of a follow along structure. 
 
 * In class we will be going over the principles listed above and you will be expected to follow along when the instructor asks you to do so.
+
+### Recursion
+* recursion is another way to represent a looping program. 
+* Simliar to a `for loop` or a `while loop` a recursive function will loop until you tell it to stop. 
+* when solving complex algorithms recursion comes in handy. It is not that great for smaller, less complex algorithms.
+ * For every recursive function, you'll need to establish what is called a `base case`. A `base case` is a condition that when triggered, will discontinue the call to your function.
---
+++ recursion.js
@@ -0,0 +1,33 @@
+// to test these problems you can run 'node recursion.js' in your terminal
+// Problem 1:
+
+let n = 1;
+while (n <= 10) {
+  console.log('While Loop', n);
+  n++;
+}
+
+// write a recursive - function called countToTen that mimics the while loop above.
+
+// code here
+
+// when you code is ready, un-comment the next line and run the file
+// console.log(countToTen());
+/* ================ Next Problem ================= */
+
+// Problem 2:
+
+const factorial = n => {
+  let result = 1;
+  for (let i = 2; i <= n; i++) {
+    result *= i;
+  }
+  return result;
+};
+
+console.log(factorial(5));
+
+// write the above functionin a recursive way.
+
+// when you code is ready, un-comment the next line and run the file
+// console.log(recursiveFactorial());
