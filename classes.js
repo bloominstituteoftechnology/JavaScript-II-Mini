6bb05577ed543c7d578d755ea6547fd52d202989
Guy A. Ross <guyallenross@gmail.com>
2017-11-09 10:39:45 -0800

    Finished it up.

---
+++ classes.js
@@ -0,0 +1,29 @@
+// to test these problems you can run 'node classes.js' in your terminal
+
+// problem #1
+// convert the Animal constructor function from 'constructors.js' into an ES6 class
+class Animal {
+    constructor (options) {
+        this.options = options;
+    }
+    grow() {
+        console.log(`${this.options.name} has grown!`);
+    }
+};
+
+// problem #2
+// convert the Cat constructor function from 'constructors.js' into an ES6 class
+class Cat extends Animal {
+    constructor (options) {
+        super(options);
+    }
+}
+
+// if everything is setup properly the code below will print 'Foofie grew larger!'
+// uncomment the code below to test your solution
+
+const foofie = new Cat({
+    name: 'foofie',
+});
+//
+foofie.grow();
