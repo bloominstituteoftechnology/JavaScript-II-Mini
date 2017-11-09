d1466aa62c53c40829fc5d99a0c1b3b59578fd89
SunJieMing <bennelson@Bens-MacBook-Pro.local>
2017-11-08 16:15:31 -0800

    Add solution

--- classes.js
+++ classes.js
@@ -2,18 +2,31 @@
 
 // problem #1
 // convert the Animal constructor function from 'constructors.js' into an ES6 class
+class Animal {
+  constructor(options) {
+    this.name = options.name;
+  }
+  grow() {
+    console.log(`${this.name} grew larger!`);
+  }
+}
 
 
 // problem #2
 // convert the Cat constructor function from 'constructors.js' into an ES6 class
 
+class Cat extends Animal {
+  meow() {
+    console.log('meow!');
+  }
+}
 
 // if everything is setup properly the code below will print 'Foofie grew larger!'
 // uncomment the code below to test your solution
 
-// const foofie = new Cat({
-//   name: 'foofie',
-// });
-//
-// foofie.grow();
+const foofie = new Cat({
+  name: 'foofie',
+});
 
+foofie.grow();
+foofie.meow();
--- constructors.js
+++ constructors.js
@@ -8,6 +8,10 @@ function Animal(options) {
   this.name = options.name;
 }
 
+Animal.prototype.grow = function() {
+  console.log(`${this.name} grew larger!`);
+};
+
 // add 'grow' to Animal's prototype here
 
 // problem #2
@@ -17,17 +21,20 @@ function Animal(options) {
 // instances of Cat should also have access to the 'grow' method
 
 function Cat(options) {
+  Animal.call(this, options);
   // invoke Animal here with .call
 }
+
+Cat.prototype = Object.create(Animal.prototype);
 
 // connect the prototypes here
 
 // if everything is setup properly the code below will print 'Foofie grew larger!'
 // uncomment the code below to test your solution
 
-// const foofie = new Cat({
-//   name: 'foofie',
-// });
-//
-// foofie.grow();
+const foofie = new Cat({
+  name: 'foofie',
+});
+
+foofie.grow();
 
