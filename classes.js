1a6c6c9cec290c293674a4c811b85495c60c1890
ubuntu <irishseanvaldivia@gmail.com>
2017-11-09 10:36:36 -0600

    Got more stuff working!

--- classes.js
+++ classes.js
@@ -2,11 +2,19 @@
 
 // problem #1
 // convert the Animal constructor function from 'constructors.js' into an ES6 class
-
+class Animal {
+  constructor(options) {
+      this.name = options.name;
+  }
+}
 
 // problem #2
 // convert the Cat constructor function from 'constructors.js' into an ES6 class
-
+class Cat extends Animal {
+    constructor(options)  {
+        
+    }
+}
 
 // if everything is setup properly the code below will print 'Foofie grew larger!'
 // uncomment the code below to test your solution
