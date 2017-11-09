19ff863a6d8169c5a16fa6a06809739ba89132ca
ubuntu <irishseanvaldivia@gmail.com>
2017-11-09 11:44:34 -0600

    Another commit

--- this.js
+++ this.js
@@ -1,10 +1,13 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. First principle: Window or console binding. When in the global scope, 'this' 
+     will refer to either the window object or the console object.
+* 2. Second principle: Implicit binding. The object before the dot in method calls
+     is 'this'.
+* 3. Third principle: New Binding; When making a new object from a constructor function
+     'this' refers the the instance of an object being created.
+* 4. Fourth principle: Explicit Binding. 'this' is explicity defined when using call() or apply()
 *
 * write out a code example of each explanation above
 */
