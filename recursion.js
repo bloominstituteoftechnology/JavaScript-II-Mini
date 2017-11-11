833258626b0b8651d6bea288ed637224ff9e865d
uohnajune <uohnajune@gmail.com>
2017-11-11 13:14:27 -0500

    Uohna's JavaScript-II-Mini

--- recursion.js
+++ recursion.js
@@ -1,33 +1,41 @@
 // to test these problems you can run 'node recursion.js' in your terminal
 // Problem 1:
 
-let n = 1;
-while (n <= 10) {
-  console.log('While Loop', n);
-  n++;
-}
+//let n = 1;
+//while (n <= 10) {
+  //console.log('While Loop', n);
+  //n++;
+//}
 
 // write a recursive - function called countToTen that mimics the while loop above.
 
-// code here
+let countToTen = num => {
+  if (num > 10) return;
+  console.log(num);
+  countToTen(++num);
+}
 
 // when you code is ready, un-comment the next line and run the file
-// console.log(countToTen());
+console.log(countToTen(1));
 /* ================ Next Problem ================= */
 
 // Problem 2:
 
-const factorial = n => {
-  let result = 1;
-  for (let i = 2; i <= n; i++) {
-    result *= i;
-  }
-  return result;
-};
+//const factorial = n => {
+  //let result = 1;
+  //for (let i = 2; i <= n; i++) {
+    //result *= i;
+  //}
+  //return result;
+//};
 
-console.log(factorial(5));
+//console.log(factorial(5));
 
-// write the above functionin a recursive way.
+let recursiveFactorial = num => {
+  if (num === 0) return 1;
+    num *= recursiveFactorial(num-1);
+    return num;
+}
 
 // when you code is ready, un-comment the next line and run the file
-// console.log(recursiveFactorial());
+console.log(recursiveFactorial(5));
--- this.js
+++ this.js
@@ -1,26 +1,49 @@
 /* The for principles of "this";
+newFunction();
+
+function newFunction() {
+/* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. The 'this' keyword defaults to the global scope which is the window or console.
+* 2. The 'this' keyword is implicitly refers to the object in which it is being called.
+* 3. When a new object is created by the 'new' keyword, the 'this' keyword refers to the new object.
+* 4. When using the 'call' or 'apply' function, the 'this' context for the 'this keyword is explicity defined.
 *
 * write out a code example of each explanation above
 */
+// Principle 1:  // code example for Window Binding: 
 
-// Principle 1
+console.log("This is:, " this);
 
-// code example for Window Binding
+// Principle 2: code example for Implicit Binding
 
-// Principle 2
+let live = {
+    money: true,
+    shop: function() {
+        if (this.money = true) {
+            console.log("Let's shop!")
+        }
+    }
+}
+live.shop();
 
-// code example for Implicit Binding
+// Principle 3: code example for New Binding
+function live () {
+    this.name = name;
+    this.money = true;
+    this.shop = function () {
+        if (this.money = true) {
+            console.log(`Let ${this.name} shop!`);
+        }
+    }
+}
+const mylive = new live ({
+    name: 'June'
+})
 
-// Principle 3
+mylive.shop();
 
-// code example for New Binding
+// Principle 4: code example for Explicit Binding
 
-// Principle 4
 
-// code example for Explicit Binding
