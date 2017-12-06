e219f45b4c9613d514139c2925927c38f53a085f
Alexander Aleksanyan <aleksanyanalek@gmail.com>
2017-12-06 11:11:55 -0800

    add and finish all assignments

--- recursion.js
+++ recursion.js
@@ -2,10 +2,19 @@
 // Problem 1:
 // hi
 
-let n = 1;
-while (n <= 10) {
-  console.log('While Loop', n);
-  n++;
+// let n = 1;
+// while (n <= 10) {
+//   console.log('While Loop', n);
+//   n++;
+// }
+
+const countToTen = (num = 1) => {
+  console.log(num);
+  if (num === 10) {
+    return "";
+  } else {
+    return countToTen(num + 1);
+  }
 }
 
 // write a recursive - function called countToTen that mimics the while loop above.
@@ -13,7 +22,7 @@ while (n <= 10) {
 // code here
 
 // when you code is ready, un-comment the next line and run the file
-// console.log(countToTen());
+console.log(countToTen());
 /* ================ Next Problem ================= */
 
 // Problem 2:
@@ -25,6 +34,16 @@ const factorial = n => {
   }
   return result;
 };
+
+
+const recursiveFactorial = (n, result = 1, i = 2) => {
+  if (i <= n) {
+    return '';
+  } else {
+    result *= i;
+    return recursiveFactorial(n + 1, result, i);
+  }
+}
 
 console.log(factorial(5));
 
--- this.js
+++ this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1.
+* 2.
+* 3.
+* 4.
 *
 * write out a code example of each explanation above
 */
@@ -15,14 +15,59 @@ console.log('hello world!');
 
 // code example for Window Binding
 
+window._ = {};
+
 // Principle 2
 
 // code example for Implicit Binding
+function foo() {
+  console.log( this.a );
+}
+
+var obj2 = {
+  a: 42,
+  foo: foo
+};
+
+var obj1 = {
+  a: 2,
+  obj2: obj2
+};
+
+obj1.obj2.foo();
 
 // Principle 3
 
 // code example for New Binding
 
+function foo(a) {
+  this.a = a;
+}
+
+var bar = new foo( 2 );
+console.log( bar.a );
+
 // Principle 4
 
 // code example for Explicit Binding
+
+function foo(something) {
+  console.log( this.a, something );
+  return this.a + something;
+}
+
+// simple `bind` helper
+function bind(fn, obj) {
+  return function() {
+    return fn.apply( obj, arguments );
+  };
+}
+
+var obj = {
+  a: 2
+};
+
+var bar = bind( foo, obj );
+
+var b = bar( 3 );
+console.log( b );
