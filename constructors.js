8d65d827eeba32b4d156fe622fad09299f990226
Terrie Boeun Kim <TERR1E@Terries-MacBook-Pro.local>
2018-01-12 04:46:55 -0800

    JavaScript-II-Mini assigment upload

--- constructors.js
+++ constructors.js
@@ -8,7 +8,10 @@ function Animal(options) {
   this.name = options.name;
 }
 
-// add 'grow' to Animal's prototype here
+Animal.prototype.grow = function() {
+  console.log(`${this.name} grew larger!`);
+};// add 'grow' to Animal's prototype here
+
 
 // problem #2
 // setup Cat to inherit from Animal
@@ -17,17 +20,17 @@ function Animal(options) {
 // instances of Cat should also have access to the 'grow' method
 
 function Cat(options) {
-  // invoke Animal here with .call
+Animal.call(this, options);  // invoke Animal here with .call
 }
 
-// connect the prototypes here
+Cat.prototype = Object.create(Animal.prototype);// connect the prototypes here
 
 // if everything is setup properly the code below will print 'Foofie grew larger!'
 // uncomment the code below to test your solution
 
-// const foofie = new Cat({
-//   name: 'foofie',
-// });
-//
-// foofie.grow();
+const foofie = new Cat({
+   name: 'foofie',
+ });
+
+ foofie.grow();
 
