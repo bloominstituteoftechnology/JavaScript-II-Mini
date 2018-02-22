abd96045207bcb17d8a43976981bc6f5f4504237
Carlos Andrade <carlos.andrade86@outlook.com>
2018-02-22 14:11:41 -0500

    WIP

--- recursion.js
+++ recursion.js
@@ -10,7 +10,12 @@ while (n <= 10) {
 
 // write a recursive - function called countToTen that mimics the while loop above.
 
-// code here
+const count = () => {
+  for (let i = 1; i <= 10; i++) {
+    console.log(i);
+  }
+}
+
 
 // when you code is ready, un-comment the next line and run the file
 // console.log(countToTen());
