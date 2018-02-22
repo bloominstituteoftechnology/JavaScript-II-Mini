925d3b97be71d47affb2e1bfdd5da2d4000bb889
Carlos Andrade <carlos.andrade86@outlook.com>
2018-02-22 18:59:10 -0500

    Carlos Andrade, Rob Rak, Viveca Valdez, Rachelle De Jesus Pestanas JavaScript-II-Mini This.js completed

--- this.js
+++ this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window binding is when you call the this. on the global scope.
+* 2. Implicit binding is when you call the this. inside of an object.
+* 3. You use the new keyword when creating an instance of an object class.
+* 4. Call/Apply temporarily binds this. to the object passed into it.
 *
 * write out a code example of each explanation above
 */
@@ -39,8 +39,12 @@ function CordialPerson(greeter) {
       console.log(this);
     };
   }
-
+  const jerry = new CordialPerson({ name: 'Newman' });
+  const newman = new CordialPerson({ name: 'Jerry' });
 // Principle 4
+
+jerry.speak.call(newman);
+newman.speak.apply(jerry);
 
 function Animal(object) {
     this.type = object.type;
@@ -50,3 +54,10 @@ function Animal(object) {
       return this.sound;
     };
   }
+
+  const doggo = new Animal({ type: 'Dog', name: 'Murphy', sound: 'Woof' });
+  const liger = new Animal({
+    type: 'LionTiger',
+    name: 'Leery',
+    sound: 'Roar/Meow'
+  });
