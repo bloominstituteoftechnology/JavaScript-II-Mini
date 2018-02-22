69d20e9029ff3e9a3e6df8757996231c852f1734
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-02-22 17:26:51 -0600

    completed factorial on recursion.js and 4 principlas of 'this' in this.js

--- this.js
+++ this.js
@@ -1,10 +1,14 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window/Global Object Binding
+    if 'this' is within a function that is being invoked, then that function's runtime environment i.e the window becomes it's context. Thus the window global object is what this points to. 
+* 2. Principle 2: Implicit Binding
+    'this' refers to the object within which 'this' may be used on a function of to set another property.
+* 3. Principle 3: New binding
+    Implies to the use of 'this' in a constructor context. 'this' refers to the object being passed out or created by the constructor. 
+* 4. Principle 4: Explicit binding
+    call or apply methods.
 *
 * write out a code example of each explanation above
 */
