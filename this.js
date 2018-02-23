5daa73676e885c132eb4ae181709e67252f1e4fd
arcadia-ego <electronics.crawford@gmail.com>
2018-02-23 07:43:22 -0800

    this.js progress

--- this.js
+++ this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Used in a "first layer" function within the global scope.
+* 2. Used with dot notation as an object method.
+* 3. Used with the new keyword, as a constructor function;
+* 4. Used with .call or .apply.
 *
 * write out a code example of each explanation above
 */
@@ -14,14 +14,35 @@ console.log('hello world!');
 // Principle 1
 
 // code example for Window Binding
+function globalWindow() {
+    console.log(this)
+}
+globalWindow();
 
 // Principle 2
-
+const dogObj = {
+    dogStatus: 'Cute',
+    reportDog: function() {
+        console.log(`This dog is ${this.dogStatus}`)
+    }
+};
+dogObj.reportDog();
 // code example for Implicit Binding
 
 // Principle 3
 
 // code example for New Binding
+function DogStatus(cuteness){
+    this.prefix = 'This dog is ';
+    this.cuteness = cuteness.level
+    this.dogCuteness = function() {
+        console.log(`This dog is ${this.cuteness}`);
+        console.log(this);
+    };
+}
+
+const uglyDog = new DogStatus({level: 'Ugly'});
+uglyDog.dogCuteness();
 
 // Principle 4
 
