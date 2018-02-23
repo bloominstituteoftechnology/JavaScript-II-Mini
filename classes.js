8a54811059cb5cbde8e0f7dad57f3ced550ed426
ubuntu <irishseanvaldivia@gmail.com>
2018-02-22 22:59:37 -0600

    Cat inherits from Animal, ES6 style. Word to tha' trizzle!

--- classes.js
+++ classes.js
@@ -13,7 +13,11 @@ class Animal {
 
 // problem #2
 // convert the Cat constructor function from 'constructors.js' into an ES6 class
-
+class Cat extends Animal {
+  constructor(options) {
+    super(options);
+  }
+}
 
 // if everything is setup properly the code below will print 'Foofie grew larger!'
 // uncomment the code below to test your solution
