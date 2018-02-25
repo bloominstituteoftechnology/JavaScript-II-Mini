f6100849f98b6dbebf207508ca6568fb50e8da53
Edward Manda <lilkups@yahoo.com>
2018-02-25 10:38:55 -0600

    Edward Manda - Johnathan Huggett - Mini JS Done!

--- classes.js
+++ classes.js
@@ -2,18 +2,35 @@
 
 // problem #1
 // convert the Animal constructor function from 'constructors.js' into an ES6 class
+class Animal {
+    constructor(options){
+        this.name = options.name;
+    }
+
+    grow() {
+        console.log(`${this.name} grew larger`);
+    }
+}
 
 
 // problem #2
 // convert the Cat constructor function from 'constructors.js' into an ES6 class
+class Cat extends Animal {
+    constructor(options) {
+        super(options);
+    }
+}
 
+const kitty = new Cat({name: 'kitty'}) ;
+kitty.grow()
 
 // if everything is setup properly the code below will print 'Foofie grew larger!'
 // uncomment the code below to test your solution
 
-// const foofie = new Cat({
-//   name: 'foofie',
-// });
-//
-// foofie.grow();
+const foofie = new Cat({
+  name: 'foofie',
+});
+
+foofie.grow();
+
 
--- constructors.js
+++ constructors.js
@@ -6,16 +6,16 @@
 
 function Animal(options) {
   this.name = options.name;
-  this.grow = function() {
-    console.log(`${this.name} grew larger`);
-  };
 }
+
+// add 'grow' to Animal's prototype here
+Animal.prototype.grow = function() {
+  console.log(`${this.name} grew larger`);
+};
 
 const ligo = new Animal({name: 'Fido'});
 
 ligo.grow();
-
-// add 'grow' to Animal's prototype here
 
 // problem #2
 // setup Cat to inherit from Animal
@@ -25,16 +25,23 @@ ligo.grow();
 
 function Cat(options) {
   // invoke Animal here with .call
+  Animal.call(this, options);
 }
 
+
 // connect the prototypes here
+Cat.prototype = Object.create(Animal.prototype);
+
+const kitty = new Cat({name: 'Kitty'});
+
+kitty.grow();
 
 // if everything is setup properly the code below will print 'Foofie grew larger!'
 // uncomment the code below to test your solution
 
-// const foofie = new Cat({
-//   name: 'foofie',
-// });
-//
-// foofie.grow();
+const foofie = new Cat({
+  name: 'foofie',
+});
+
+foofie.grow();
 
