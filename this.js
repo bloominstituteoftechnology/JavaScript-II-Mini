ef1bb3e2ce1da78ac5a688c38e55153be6ee94ca
Devon Bacon <devon.bacon@gmail.com>
2018-03-21 12:49:32 -0700

    Answered question 3 in part 1 of this.js

--- this.js
+++ this.js
@@ -1,9 +1,9 @@
 /* The four principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. Window Binding: Binds to what is usually the window object that executes you code. Ultimately it binds to the global object.
-* 2. Implicit Binding: When applying a method this pertains to what is left of the dot.
-* 3. 
+* 1. Window Binding: In this case 'this' is usually the window object that executes you code. Ultimately it binds to the global object.
+* 2. Implicit Binding: When applying a method 'this' pertains to what is left of the dot.
+* 3. New Binding: When creating something using the 'new' keyword 'this' related to the newly created data.
 * 4. 
 *
 * write out a code example of each explanation above
