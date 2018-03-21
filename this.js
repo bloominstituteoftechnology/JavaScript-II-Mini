c4b436455491eb4adff8bd26a176c764ed04e07d
Devon Bacon <devon.bacon@gmail.com>
2018-03-21 12:50:26 -0700

    Answered question 4 in part 1 of this.js

--- this.js
+++ this.js
@@ -4,25 +4,17 @@
 * 1. Window Binding: In this case 'this' is usually the window object that executes you code. Ultimately it binds to the global object.
 * 2. Implicit Binding: When applying a method 'this' pertains to what is left of the dot.
 * 3. New Binding: When creating something using the 'new' keyword 'this' related to the newly created data.
-* 4. 
+* 4. Explicit Binding: This type ofbinding happens when using .call() and .apply(). This is very similar to implicit binding but essentially reorients the focus of 'this'.
 *
 * write out a code example of each explanation above
 */
 
 console.log('hello world!');
 
-// Principle 1
+// Principle 1: code example for Window Binding
 
-// code example for Window Binding
+// Principle 2: code example for Implicit Binding
 
-// Principle 2
+// Principle 3: code example for New Binding
 
-// code example for Implicit Binding
-
-// Principle 3
-
-// code example for New Binding
-
-// Principle 4
-
-// code example for Explicit Binding
+// Principle 4: code example for Explicit Binding
