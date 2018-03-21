5a7bd4147e269998ac7d690fdae16b9d5234a148
Kevin Tran <supertatortots@gmail.com>
2018-03-21 16:33:05 -0400

    completed count to 10

--- recursion.js
+++ recursion.js
@@ -1,31 +1,37 @@
 // to test these problems you can run 'node recursion.js' in your terminal
 // Problem 1:
 
-let n = 1;
-while (n <= 10) {
-  console.log('While Loop', n);
-  n++;
-}
+// let n = 1;
+// while (n <= 10) {
+//   console.log('While Loop', n);
+//   n++;
+// }
 
 // write a recursive - function called countToTen that mimics the while loop above.
 
 // code here
+let countToTen = (num) => {
+  if (num === 11) return; //base case;
+  console.log(num)
+  countToTen(num + 1);
+}
+// countToTen(2);
 
 // when you code is ready, un-comment the next line and run the file
-// console.log(countToTen());
+ console.log(countToTen(1));
 /* ================ Next Problem ================= */
 
 // Problem 2:
-
-const factorial = n => {
-  let result = 1;
-  for (let i = 2; i <= n; i++) {
-    result *= i;
-  }
-  return result;
-};
-
-console.log(factorial(5));
+//
+// const factorial = n => {
+//   let result = 1;
+//   for (let i = 2; i <= n; i++) {
+//     result *= i;
+//   }
+//   return result;
+// };
+//
+// console.log(factorial(5));
 
 // write the above function in a recursive way.
 
