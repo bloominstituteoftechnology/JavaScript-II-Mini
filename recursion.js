ed8b3726937fbdd88d9d072abcea3d3e9c123078
Natalie Schennum <natalieschennum@gmail.com>
2018-03-21 14:41:44 -0700

    Recursion and this

--- recursion.js
+++ recursion.js
@@ -2,29 +2,40 @@
 // Problem 1:
 //Note for initial PR
 
-let n = 1;
-while (n <= 10) {
-  console.log('While Loop', n);
-  n++;
-}
+// let n = 1;
+// while (n <= 10) {
+//  console.log('While Loop', n);
+//  n++;
+//}
+const countToTen = num => {
+  if (num > 10) return; 
+  console.log(num); 
+  return countToTen(++num); 
+};
+let num = 1; 
 
 // write a recursive - function called countToTen that mimics the while loop above.
 
 // code here
 
 // when you code is ready, un-comment the next line and run the file
-// console.log(countToTen());
+console.log(countToTen(num));
 /* ================ Next Problem ================= */
 
 // Problem 2:
 
 const factorial = n => {
-  let result = 1;
-  for (let i = 2; i <= n; i++) {
-    result *= i;
-  }
-  return result;
+  if (n === 0) return 0;
+  if (n === 1) return 1; 
+  else {return n * factorial(n - 1)};  
 };
+
+ // let result = 1;
+ // for (let i = 2; i <= n; i++) {
+ //   result *= i;
+ // }
+ // return result;
+//};
 
 console.log(factorial(5));
 
--- this.js
+++ this.js
@@ -1,28 +1,76 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window Binding will cause the 'this' function to refer to 
+the applicaiton frame of wherever you are executing the funciton. 
+* 2. Implicit Binding uses this usually FOLLOWED by a dot and then 
+an identifier that refers to a specific variable to tie into the function. 
+* 3. New Binding is used to create a bound function that can have new objects 
+passed in via 'this'. It can be used with constructor functions in object oriented 
+programming to tie changable variables into a related function.
+* 4. Explicit Binding can be used to override constructor objects by calling the 
+funciton explicitly with .call and .apply.  These fucntions are very similar except that .call uses a (...__) format. 
 *
 * write out a code example of each explanation above
 */
 
-console.log('hello world!');
+// console.log('hello world!');
 
 // Principle 1
 
 // code example for Window Binding
-
+function sayName1(name) {
+    console.log(this); // will not refer to Natalie!!!
+    return name;
+  }
+  sayName1("Natalie");
 // Principle 2
 
 // code example for Implicit Binding
-
+const sayNameFunc = obj => {
+    obj.sayName = function() {
+      console.log(`Hello my name is ${this.name}`);
+      console.log(this);
+    };
+  };
+  
+  const me = { name: 'Natalie' };
+  const you = { name: 'Kia' };
+  sayNameFunc(me);
+  sayNameFunc(you);
 // Principle 3
 
 // code example for New Binding
+let myinfo = {
+    name: 'Natalie',
+    age: 31
+  };
+  
+  let sayName = function(hobby1, hobby2) {
+    console.log('My name is ' + this.name + ', I like to ' + hobby1 + ' and ' + hobby2 + '.');
+  };
+   let sayAge = function(hobby1, hobby2) {
+    console.log('My age is ' + this.age + ', I still like to ' + hobby1 + ' and ' + hobby2 + '.');
+  };
+  
+let hobbies = ['study Biology','Cook'];
+let newFunction = sayName.bind(myinfo, ...hobbies);
+let ageFunc = sayAge.bind(myinfo, ...hobbies); 
+newFunction(); 
+ageFunc(); 
 
 // Principle 4
 
 // code example for Explicit Binding
+/*let myinfo2 = {
+    name: 'Natalie',
+    age: 31
+  };*/
+  
+  let sayName2 = function(hobby1, hobby2) {
+    console.log('My name is ' + this.name + ', I like to ' + hobby1 + ' and ' + hobby2 + '.');
+  }
+  
+ // let hobbies = ['study Biology','Cook'];
+  
+  sayName2.apply(myinfo, hobbies);
