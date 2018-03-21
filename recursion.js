7d5b59ac39535fcb77aaa3ca86f277f4019e33d7
Abraham Bueno <abueno@localhost.localdomain>
2018-03-21 18:03:33 -0500

    Completed assignments for day: recursions and this.js. Need to edit some things on this.js

--- recursion.js
+++ recursion.js
@@ -10,9 +10,16 @@ while (n <= 10) {
 // write a recursive - function called countToTen that mimics the while loop above.
 
 // code here
+let m = 1;
+let countToTen = function(m) {
+  if (m === 10) return console.log('While Loop II', m);
+  console.log('While Loop II', m);
+  m++;
+  return countToTen(m);
+}
 
 // when you code is ready, un-comment the next line and run the file
-// console.log(countToTen());
+console.log(countToTen(m));
 /* ================ Next Problem ================= */
 
 // Problem 2:
@@ -20,7 +27,7 @@ while (n <= 10) {
 const factorial = n => {
   let result = 1;
   for (let i = 2; i <= n; i++) {
-    result *= i;
+    result *= i; // 1 * 2, 2 * 3, 6 * 4, 24 * 5
   }
   return result;
 };
@@ -28,6 +35,18 @@ const factorial = n => {
 console.log(factorial(5));
 
 // write the above function in a recursive way.
+let fresult = 1;
+let recursiveFactorial = (k) => {
+  if (k === 1) return console.log(fresult);
+  fresult *= k;
+  // console.log("K is: " + k + "fresult: " +fresult);
+  k--;
+  recursiveFactorial(k);
+
+}
+// factorialT(5);
+
+
 
 // when your code is ready, un-comment the next line and run the file
-// console.log(recursiveFactorial());
+console.log(recursiveFactorial(5));
