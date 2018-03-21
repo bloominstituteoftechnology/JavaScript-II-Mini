f702a47c52e29e10256ed3c56fcf800013f888b9
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-03-21 16:59:11 -0700

    Completed JavaScript-II-Mini Proj

--- recursion.js
+++ recursion.js
@@ -1,28 +1,49 @@
 // to test these problems you can run 'node recursion.js' in your terminal
 // Problem 1:
 
-let n = 1;
-while (n <= 10) {
-  console.log('While Loop', n);
-  n++;
-}
+// let n = 1;
+// while (n <= 10) {
+//   console.log('While Loop', n);
+//   n++;
+// }
 
 // write a recursive - function called countToTen that mimics the while loop above.
 
 // code here
+let n = 1;
+
+const countToTen = () => {
+  if (n > 10) {
+    return;
+  }
+  console.log(n);
+  return countToTen(++n);
+};
 
 // when you code is ready, un-comment the next line and run the file
-// console.log(countToTen());
+console.log(countToTen());
+
 /* ================ Next Problem ================= */
 
 // Problem 2:
 
+// const factorial = n => {
+//   let result = 1;
+//   for (let i = 2; i <= n; i++) {
+//     result *= i;
+//   }
+//   return result;
+// };
+
+// console.log(factorial(5));
+
 const factorial = n => {
-  let result = 1;
-  for (let i = 2; i <= n; i++) {
-    result *= i;
+  //base case
+  if (n === 0) {
+    return 1;
   }
-  return result;
+  //return fuction
+  return n * factorial(n - 1);
 };
 
 console.log(factorial(5));
@@ -30,4 +51,5 @@ console.log(factorial(5));
 // write the above function in a recursive way.
 
 // when your code is ready, un-comment the next line and run the file
-// console.log(recursiveFactorial());
+//console.log(recursiveFactorial());
+//left the above commented out since it wasn't returning output without errors??
--- this.js
+++ this.js
@@ -1,10 +1,11 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window Binding - If none of the below rules for implicit, new binding or explicit apply, 'this' will be defaulted to the window object unless you use strict mode.
+* 2. Implicit Binding - Look to the left of the dot of your invocation (ex: dog.sayName();) so dog is what's being called out. 
+* 3. New Binding - When you invoke a function as a new keyword, 'this' keyword inside the function is tied to your new object. 
+* 4. Explicit Binding - When you are explicitly stating the 'this' by calling it out (ex: sayName.call(dog));) so in this case you're calling out dog explicitly. It tells the
+fucntion what context 'this' will be used for by utilizing call, apply or bind.
 *
 * write out a code example of each explanation above
 */
@@ -12,17 +13,56 @@
 console.log('hello world!');
 
 // Principle 1
-
 // code example for Window Binding
+var sayAge = function(){
+    console.log(this.age);
+  };
+  
+  var me = {
+    age: 32
+  };
+  
+  sayAge.call(me);
 
 // Principle 2
-
 // code example for Implicit Binding
+var dog = {
+    name: 'Comrade Puggington',
+    age: 11,
+    breed: 'pug',
+    sayName: function() {
+        console.log(this.breed);
+    }
+};
+dog.sayName();
 
 // Principle 3
-
 // code example for New Binding
+// Dog is capitalized to show this is a constructor function
+function HappyDog(pug) {
+    this.greeting = 'Bark ';
+    this.pug = pug;
+    this.speak = function() {
+      console.log(this.greeting + this.pug);
+      console.log(this);
+    };
+}
+
+const sassy = new HappyDog('Comrade');
+const comrade = new HappyDog('Sassy');
+
+comrade.speak();
+sassy.speak();
 
 // Principle 4
-
 // code example for Explicit Binding
+var sayName = function() {
+    console.log('My name is ' + this.name);
+};
+
+var dog = {
+    name: 'Comrade Puggington', 
+    breed: 'pug',
+};
+
+sayName.call(dog);
