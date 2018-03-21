a83296b4a08f49dce119ac8bd94638c488bbc600
Robert Hogan <rhogan29@outlook.com>
2018-03-21 17:07:27 -0400

    Robert Hogan. JavaScript-II-Mini Assignment completed. All problems complete.

--- recursion.js
+++ recursion.js
@@ -11,13 +11,19 @@ while (n <= 10) {
 
 // code here
 
+const countToTen = (n) => {
+  console.log('While Loop', n);
+  while (n < 10) {
+    return countToTen(n + 1);
+  }
+}
 // when you code is ready, un-comment the next line and run the file
-// console.log(countToTen());
+ console.log(countToTen(1));
 /* ================ Next Problem ================= */
 
 // Problem 2:
 
-const factorial = n => {
+const factorial = n => { // function named 1
   let result = 1;
   for (let i = 2; i <= n; i++) {
     result *= i;
@@ -29,5 +35,13 @@ console.log(factorial(5));
 
 // write the above function in a recursive way.
 
+const recursiveFactorial = (n) => { // function called recursiveFactorial
+  if (n === 0 || n === 1) { // base case of if n is either 0 or 1, the factorial of both is 1. 
+    return 1; //  return 1 for either case. 
+  } else { 
+    return n * recursiveFactorial(n - 1) //return n x the function called with n -1. n = 4,  4 * (4 - 1) = 12. 
+  }
+}
+
 // when your code is ready, un-comment the next line and run the file
-// console.log(recursiveFactorial());
+ console.log(recursiveFactorial(4));
