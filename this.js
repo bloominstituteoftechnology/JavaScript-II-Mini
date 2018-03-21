77a3f29fead3a2cb26f836fb20ba091cdcc7acb0
Matthias Bakken <matthias.bakken@gmail.com>
2018-03-21 14:14:17 -0700

    finished this and recursive

--- this.js
+++ this.js
@@ -2,9 +2,9 @@
 * in your own words. explain the four principle for the "this" keyword below.
 *
 * 1. The global scope of this is calling on properties of the window object.
-* 2. Explicit -
-* 3. Implicit - calling from within
-* 4. new - 
+* 2. Explicit - calling from another function to a specified global object
+* 3. Implicit - calling from within object
+* 4. new - building a new object using another objects parameters for creating new objects properties
 *
 * write out a code example of each explanation above
 */
