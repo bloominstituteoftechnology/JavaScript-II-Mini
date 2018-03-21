fc504326ac933ddac612671b2747eb561de23e8e
cgoez <goez2001@gmail.com>
2018-03-21 13:13:57 -0700

    recursion done

--- recursion.js
+++ recursion.js
@@ -10,9 +10,15 @@ while (n <= 10) {
 // write a recursive - function called countToTen that mimics the while loop above.
 
 // code here
+let num = 1;
+const countToTen = num => {
+  if (num > 10) return ; // base
+  console.log(num);
+  return countToTen(++num);
+}
 
 // when you code is ready, un-comment the next line and run the file
-// console.log(countToTen());
+console.log(countToTen(num));
 /* ================ Next Problem ================= */
 
 // Problem 2:
@@ -28,6 +34,10 @@ const factorial = n => {
 console.log(factorial(5));
 
 // write the above function in a recursive way.
+const recursiveFactorial = n => {
+  if (n === 0) return 1; // base
+  return n * recursiveFactorial(n - 1);
+};
 
 // when your code is ready, un-comment the next line and run the file
-// console.log(recursiveFactorial());
+console.log(recursiveFactorial()); // make sure n is number or things break!
