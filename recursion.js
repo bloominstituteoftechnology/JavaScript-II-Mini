d288225c6faa82b4fd8cff90877c10e35e637dd3
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-03-21 16:10:51 -0500

    completed this.jsand recursion.js

--- recursion.js
+++ recursion.js
@@ -10,6 +10,11 @@ while (n <= 10) {
 // write a recursive - function called countToTen that mimics the while loop above.
 
 // code here
+function countToTen(n) {
+  if (n > 10) return n;
+  console.log(n);
+  countToTen(++n);
+}
 
 // when you code is ready, un-comment the next line and run the file
 // console.log(countToTen());
@@ -17,17 +22,19 @@ while (n <= 10) {
 
 // Problem 2:
 
-const factorial = n => {
-  let result = 1;
-  for (let i = 2; i <= n; i++) {
-    result *= i;
-  }
-  return result;
-};
 
-console.log(factorial(5));
+
+// console.log(factorial(5));
 
 // write the above function in a recursive way.
 
+const recursiveFactorial = (n) => {
+  let result = 1;
+  if (n === 1 || n === 0) return n;
+  result *= n * recursiveFactorial(n - 1); // 5 * 4 * 3 * 2
+  return result;
+};
+
+
 // when your code is ready, un-comment the next line and run the file
-// console.log(recursiveFactorial());
+console.log(recursiveFactorial(10));
--- this.js
+++ this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. implicit - when used from within an object, this refers to the object in which it is being used
+* 2. explicit - .call , .apply, .bind
+* 3. window/global
+* 4. new keyword - when used in a constructor function, this refers to the new object that will be created using the constructor
 *
 * write out a code example of each explanation above
 */
@@ -14,15 +14,38 @@ console.log('hello world!');
 // Principle 1
 
 // code example for Window Binding
+console.log(this);
 
 // Principle 2
 
 // code example for Implicit Binding
+const me = {
+    name: 'Iqra',
+    sayHello: function() {
+        console.log('Hi ' + this.name)
+    }
+}
 
 // Principle 3
 
 // code example for New Binding
+const User = function (username, password) {
+    this.username = username;
+    this.password = password;
+}
+const newUser1 = new User ('iqra', 'shjkhsf');
+console.log(newUser1);
 
 // Principle 4
 
 // code example for Explicit Binding
+
+const myPet = {
+    name: 'Simba',
+    age: 5
+}
+
+function myCat() {
+    console.log(`my cat's name is ${this.name}`)
+}
+myCat.call(myPet);
