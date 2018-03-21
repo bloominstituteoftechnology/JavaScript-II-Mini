5751068787d8f99bd416c5e8365cc2f574890c6b
Boredom <macnwandu@gmail.com>
2018-03-21 18:04:04 -0500

    mini challenge

--- recursion.js
+++ recursion.js
@@ -1,23 +1,32 @@
 // to test these problems you can run 'node recursion.js' in your terminal
 // Problem 1:
 
-let n = 1;
+/*let n = 1;
 while (n <= 10) {
   console.log('While Loop', n);
   n++;
-}
+} */
 
 // write a recursive - function called countToTen that mimics the while loop above.
 
 // code here
+let n = 1;
+  
+const countToTen = () => {
+  while ( n > 9 ) {
+    return 10;
+  }
+    console.log(n);
+   return countToTen(n++);
+};
 
 // when you code is ready, un-comment the next line and run the file
-// console.log(countToTen());
+console.log(countToTen());
 /* ================ Next Problem ================= */
 
 // Problem 2:
 
-const factorial = n => {
+/* const factorial = n => {
   let result = 1;
   for (let i = 2; i <= n; i++) {
     result *= i;
@@ -25,9 +34,16 @@ const factorial = n => {
   return result;
 };
 
-console.log(factorial(5));
+console.log(factorial(5)); */
 
 // write the above function in a recursive way.
+const recursiveFactorial = (n) => {
+  var nextNum = n - 1;
+  if (n === 1) {
+    return n;
+  }
+  return n * recursiveFactorial(nextNum);
+}
 
 // when your code is ready, un-comment the next line and run the file
-// console.log(recursiveFactorial());
+console.log(recursiveFactorial(5));
