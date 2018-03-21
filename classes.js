ec2d3b87e973d61440148884b34f0038353add9c
K Haag <kh@Ks-MacBook-Pro.local>
2018-03-21 15:13:59 -0700

    JSII-Mini P2

--- classes.js
+++ classes.js
@@ -1,6 +1,6 @@
 // to test these problems you can run 'node classes.js' in your terminal
 
-
+// Do Mini First
 // problem #1
 // convert the Animal constructor function from 'constructors.js' into an ES6 class
 
