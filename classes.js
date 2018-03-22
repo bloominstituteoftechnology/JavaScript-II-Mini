0efc66401aa7a29ac23f995cd0a7ec644ffe9f90
imsdhk <jappleleaf@gmail.com>
2018-03-22 15:24:31 -0400

    finished converting classess.js into es6 syntax, used super to replace call and Object.create methods

--- classes.js
+++ classes.js
@@ -2,18 +2,29 @@
 
 // problem #1
 // convert the Animal constructor function from 'constructors.js' into an ES6 class
-
+class Animal {
+    constructor(params) {
+        this.name = params.name;
+    }
+    grow () {
+        `${this.name} grew larger!`;
+    }
+} 
 
 // problem #2
 // convert the Cat constructor function from 'constructors.js' into an ES6 class
 
-
+class Cat extends Animal {
+    constructor(params) {
+        super(params); 
+    }
+}
 // if everything is setup properly the code below will print 'Foofie grew larger!'
 // uncomment the code below to test your solution
 
-// const foofie = new Cat({
-//   name: 'foofie',
-// });
-//
-// foofie.grow();
+const foofie = new Cat({
+  name: 'foofie',
+});
+
+foofie.grow();
 
