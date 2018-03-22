530c6c990a8c6ce8411d00eff7c4697b589cb40e
Devon Bacon <devon.bacon@gmail.com>
2018-03-22 10:48:45 -0700

    Completed problem 2 in classes.js

--- classes.js
+++ classes.js
@@ -14,14 +14,18 @@ class Animal {
 
 // problem #2
 // convert the Cat constructor function from 'constructors.js' into an ES6 class
-
+class Cat extends Animal {
+  constructor(options) {
+    super(options);
+  }
+}
 
 // if everything is setup properly the code below will print 'Foofie grew larger!'
 // uncomment the code below to test your solution
 
-// const foofie = new Cat({
-//   name: 'foofie',
-// });
-//
-// foofie.grow();
+const foofie = new Cat({
+  name: 'foofie',
+});
+
+foofie.grow();
 
