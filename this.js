8c9189f307ec38909f8bb6eba4830811861fda1a
faulkenfett11 <36997363+faulkenfett11@users.noreply.github.com>
2018-03-21 20:08:58 -0700

    answered .this questions

--- this.js
+++ this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window Binding refers to a global context, none of the rules for the other bindings apply.
+* 2. Implicit Binding refers to anything left of the .this
+* 3. In explicit binding you use .call, .apply, or .bind to call an object
+* 4. New binding is when we assign new objects to a function
 *
 * write out a code example of each explanation above
 */
