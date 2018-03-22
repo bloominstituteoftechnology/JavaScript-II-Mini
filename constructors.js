03ac1b982e4dc157214b627722bb723bd27c6563
Kevin Tran <supertatortots@gmail.com>
2018-03-22 15:16:25 -0400

    changed console.log to return

--- constructors.js
+++ constructors.js
@@ -9,7 +9,7 @@ function Animal(options) {
 }
 // add 'grow' to Animal's prototype here
 Animal.prototype.grow = function() {
-  console.log(`${this.name} grew larger!`);
+  return `${this.name} grew larger!`;
 };
 // problem #2
 // setup Cat to inherit from Animal
