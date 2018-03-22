fd83200ff04f94d0a747dbe6b294d5343722ecdf
imsdhk <jappleleaf@gmail.com>
2018-03-22 15:07:15 -0400

    finished constructors.js passed the test

--- constructors.js
+++ constructors.js
@@ -8,7 +8,11 @@ function Animal(options) {
   this.name = options.name;
 }
 
+
 // add 'grow' to Animal's prototype here
+Animal.prototype.grow = function(){
+  console.log(`${this.name} grew larger!`);
+}
 
 // problem #2
 // setup Cat to inherit from Animal
@@ -18,16 +22,17 @@ function Animal(options) {
 
 function Cat(options) {
   // invoke Animal here with .call
+  Animal.call(this,options);
 }
 
 // connect the prototypes here
-
+Cat.prototype = createObject(Animal.prototype); 
 // if everything is setup properly the code below will print 'Foofie grew larger!'
 // uncomment the code below to test your solution
 
-// const foofie = new Cat({
-//   name: 'foofie',
-// });
-//
-// foofie.grow();
+const foofie = new Cat({
+  name: 'foofie',
+});
+
+foofie.grow();
 
