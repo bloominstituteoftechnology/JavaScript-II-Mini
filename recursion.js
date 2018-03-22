52489c6676e43338e0ab67567364e879163e20f6
Jasmin Robinson <jasminrobinson@Jasmins-MacBook-Pro.local>
2018-03-22 19:05:23 -0400

    90% completed work

--- recursion.js
+++ recursion.js
@@ -10,24 +10,37 @@ while (n <= 10) {
 // write a recursive - function called countToTen that mimics the while loop above.
 
 // code here
+const countToTen = (n)
+if (n > 10)
+returnconsole.log(n)
+return countToTen(++n){
+  
+  }
+}
 
 // when you code is ready, un-comment the next line and run the file
 // console.log(countToTen());
 /* ================ Next Problem ================= */
 
 // Problem 2:
-
-const factorial = n => {
-  let result = 1;
-  for (let i = 2; i <= n; i++) {
-    result *= i;
-  }
   return result;
-};
+  };
+ 
 
-console.log(factorial(5));
+ const recursiveFactorial = (n, result = 1, i = 2) => {
+   if (i <= n) {
+     return '';
+   } else {
+     result *= i;
+     return recursiveFactorial(n + 1, result, i);
+   }
+ }
+
 
 // write the above function in a recursive way.
 
 // when your code is ready, un-comment the next line and run the file
 // console.log(recursiveFactorial());
+
+
+
--- this.js
+++ this.js
@@ -14,15 +14,58 @@ console.log('hello world!');
 // Principle 1
 
 // code example for Window Binding
+ window._ = {};
 
 // Principle 2
 
 // code example for Implicit Binding
+function foo() {
+      console.log( this.a );
+    }
+    
+    var obj2 = {
+      a: 42,
+      foo: foo
+    };
+    
+    var obj1 = {
+      a: 2,
+      obj2: obj2
+    };
+    +
+    +obj1.obj2.foo();
+          
 
 // Principle 3
 
 // code example for New Binding
+ function foo(a) {
+       this.a = a;
+     }
+    
+     var bar = new foo( 2 );
+     console.log( bar.a );
 
 // Principle 4
 
 // code example for Explicit Binding
+ function foo(something) {
+       console.log( this.a, something );
+       return this.a + something;
+     }
+    
+     // simple `bind` helper
+     function bind(fn, obj) {
+       return function() {
+         return fn.apply( obj, arguments );
+       };
+     }
+    
+     var obj = {
+       a: 2
+     };
+    
+     var bar = bind( foo, obj );
+    
+     var b = bar( 3 );
+     console.log( b );
