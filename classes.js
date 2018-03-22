a7671540c8d7ffb2cb60f85d9d1769f39dd93874
Jonathan Greene <greeneselectronics@gmail.com>
2018-03-22 17:15:01 -0600

    Completed classes and constructors for the mini

--- classes.js
+++ classes.js
@@ -2,7 +2,39 @@
 
 // problem #1
 // convert the Animal constructor function from 'constructors.js' into an ES6 class
+// to test these problems you can run 'node constructors.js' in your terminal
+class Animal{
+    constructor(options) {
+        this.name = options.name;
+    
+    }
 
+    grow() {
+    
+        console.log(this.name + ' grew larger');
+      
+    }
+}
+
+  const options = {  // creates a new object called options
+    name: 'sally',
+  }
+  
+   let Cheetah = new Animal(options); // creates a new object of animal type
+  Cheetah.grow();
+  
+  // problem #2
+  // setup Cat to inherit from Animal
+  // the Animal constructor needs to be invoked with the 'options' argument
+  // Cat should have its prototype inherit from Animal
+  // instances of Cat should also have access to the 'grow' method
+  
+  class Cat extends Animal {
+  constructor(options) {
+      super(options)
+    // invoke Animal here with .call
+  }
+}
 
 // problem #2
 // convert the Cat constructor function from 'constructors.js' into an ES6 class
@@ -11,9 +43,9 @@
 // if everything is setup properly the code below will print 'Foofie grew larger!'
 // uncomment the code below to test your solution
 
-// const foofie = new Cat({
-//   name: 'foofie',
-// });
-//
-// foofie.grow();
+const foofie = new Cat({
+  name: 'foofie',
+});
+
+foofie.grow();
 
