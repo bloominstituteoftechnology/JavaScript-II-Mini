a220689a5217160476cc5efa480a141fffedc06a
David Finley <dfinley2917@gmail.com>
2018-03-22 10:32:13 -0400

    recursion and this complete

--- recursion.js
+++ recursion.js
@@ -1,18 +1,25 @@
 // to test these problems you can run 'node recursion.js' in your terminal
 // Problem 1:
 
-let n = 1;
-while (n <= 10) {
-  console.log('While Loop', n);
-  n++;
-}
+// let n = 1;
+// while (n <= 10) {
+//   console.log('While Loop', n);
+//   n++;
+// }
 
 // write a recursive - function called countToTen that mimics the while loop above.
 
 // code here
+const countToTen = num => {
+  if (num > 10) return;
+  console.log(num);
+  return countToTen(++num); 
+};
+
+
 
 // when you code is ready, un-comment the next line and run the file
-// console.log(countToTen());
+console.log(countToTen(1));
 /* ================ Next Problem ================= */
 
 // Problem 2:
@@ -28,6 +35,11 @@ const factorial = n => {
 console.log(factorial(5));
 
 // write the above function in a recursive way.
-
+const recursiveFactorial = n => {
+  if (n === 0) {
+    return 1;
+  }
+  return n * recursiveFactorial(n - 1);
+}
 // when your code is ready, un-comment the next line and run the file
-// console.log(recursiveFactorial());
+console.log(recursiveFactorial(5));
--- this.js
+++ this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. using THIS within a function is a window object when using Global functions.  In essence the function will be a method.
+* 2. using 'this.*' within a function references the object's variable that is defined earlier in the function  
+* 3. using 'this.' followed by an object and its argument to will be created within a Constructor function. 
+* 4. using '.call' or '.apply' method followed by an argument passes the value of THIS from one context to another..
 *
 * write out a code example of each explanation above
 */
@@ -14,15 +14,40 @@ console.log('hello world!');
 // Principle 1
 
 // code example for Window Binding
-
+function greetMe(name) {
+    console.log('Hello ' + name);
+    console.log(this);
+}
+greetMe('Dave');
 // Principle 2
 
 // code example for Implicit Binding
+var greetMe = {
+    greeting: 'Hello ',
+    speak: function(name) {
+        console.log(this.greeting + name);
+        console.log(this);
+    }
+}
+greetMe.speak('Dave');
 
+var greeting = 'Howdy ',
+    greet = greetMe.speak;
+
+greet('Dave');
 // Principle 3
 
 // code example for New Binding
+GreetMe.prototype.sayGoodbye = function() {
+    console.log('Goodbye ' + this.name);
+    console.log(this);
+};
+
+greetDave.sayGoodbye();
+greetHomer.sayGoodbye();
 
 // Principle 4
 
 // code example for Explicit Binding
+greetDave.sayGoodbye.call(greetDave);
+greetHomer.sayGoodbye.apply(greetHomer);
