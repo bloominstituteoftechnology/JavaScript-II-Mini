c507326e3cbe1684bc6a55426b9ad0c94399a103
M. Margo Nikolaisen <mm.nikolaisen@gmail.com>
2018-03-21 18:00:52 -0600

    Still Working On This

--- this.js
+++ this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window Binding - .this keyword defaults to the window object.
+* 2. Implicit Binding - References whatever is listed to the left of the dot.
+* 3. New Binding - a function invoked with a new keyword.
+* 4. Explicit Binding - .call, .apply, .bind.
 *
 * write out a code example of each explanation above
 */
@@ -19,9 +19,24 @@ console.log('hello world!');
 
 // code example for Implicit Binding
 
+let Example = function (name, gender) {
+    return {
+        name: name,
+        gender: gender,
+        sayName: function () {
+
+        }
+
+    }
+}
+ name.Example;
+
+
 // Principle 3
 
 // code example for New Binding
+
+
 
 // Principle 4
 
