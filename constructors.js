e9ea3b73b13b5f1c56f040310f27671fd88e5b35
Courtney Buratto <you@example.com>
2018-08-21 23:07:22 -0600

    initial commit

--- constructors.js
+++ constructors.js
@@ -9,6 +9,11 @@ function Animal(options) {
 }
 
 // add 'grow' to Animal's prototype here
+Animal.prototype.grow = function() {
+  console.log(`${this.name} grew larger!`)
+}
+
+
 
 // problem #2
 // setup Cat to inherit from Animal
